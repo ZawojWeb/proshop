4e61eddcb2375f5382922034507b5850f007b3ac
Kacper Zawojski <zawojweb@gmail.com>
2021-10-21 22:08:38 +0200

    Finish fornt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,20 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome in proshop</h1>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
