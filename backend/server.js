d347298b5be1d679a207935cecfdfdba0da74de0
Kacper Zawojski <zawojweb@gmail.com>
2021-10-24 23:09:09 +0200

    Change color in terminal

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import products from './data/products.js'
 import connectDB from './config/db.js'
-
+import colors from 'colors'
 dotenv.config()
 
 connectDB()
@@ -22,4 +22,4 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold))
